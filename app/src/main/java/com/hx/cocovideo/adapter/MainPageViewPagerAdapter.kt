12a4231ae6a88a3b5c3b43c542da28082569c6b8
henry_zhang <zhangheng_mx@outlook.com>
2018-12-19 21:42:04 +0800

    首页代码同步(底部导航代码，未写完)

---
+++ app/src/main/java/com/hx/cocovideo/adapter/MainPageViewPagerAdapter.kt
@@ -0,0 +1,15 @@
+package com.hx.cocovideo.adapter
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+
+class MainPageViewPagerAdapter(fm: FragmentManager, fragmrntList : MutableList<Fragment>) : FragmentPagerAdapter(fm) {
+    override fun getCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItem(position: Int): Fragment {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
---
+++ app/src/main/java/com/hx/cocovideo/fragment/TVFragment.kt
@@ -0,0 +1,20 @@
+package com.hx.cocovideo.fragment
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.hx.cocovideo.R
+
+class TVFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_tv, container, false)
+    }
+
+
+}
