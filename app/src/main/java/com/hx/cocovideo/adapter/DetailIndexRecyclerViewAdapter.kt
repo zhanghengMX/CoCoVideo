227a75837f4bf8cd15bdc17423cd50c8b45c2e04
zhangheng <487225093@qq.com>
2019-01-14 18:12:44 +0800

    详情页剧集选中样式

--- app/src/main/java/com/hx/cocovideo/adapter/DetailIndexRecyclerViewAdapter.kt
+++ app/src/main/java/com/hx/cocovideo/adapter/DetailIndexRecyclerViewAdapter.kt
@@ -1,5 +1,6 @@
 package com.hx.cocovideo.adapter
 
+import android.icu.text.SelectFormat
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -20,6 +21,12 @@ import com.hx.cocovideo.R
 class DetailIndexRecyclerViewAdapter(var datas : MutableList<VideoUrl>) : RecyclerView.Adapter<DetailIndexRecyclerViewAdapter.CategoryPageViewHolder>() {
 
     private var onclickListener : RecyclerOnItemClickListener<VideoUrl>? = null
+
+    private var selectedPosition = 0
+
+    fun setSelectedPosition(position: Int) {
+        selectedPosition = position
+    }
 
     fun setOnClickListener(listener : RecyclerOnItemClickListener<VideoUrl>) {
         onclickListener = listener
@@ -42,7 +49,19 @@ class DetailIndexRecyclerViewAdapter(var datas : MutableList<VideoUrl>) : Recycl
     override fun onBindViewHolder(holder: CategoryPageViewHolder, position: Int) {
         val videoUrl = datas[position]
         holder.videoIndexTv.text = videoUrl.index.toString()
-        holder.itemView.setOnClickListener { onclickListener?.onClick(videoUrl) }
+        holder.itemView.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(itemView: View?) {
+                selectedPosition = position
+                notifyDataSetChanged()
+                onclickListener?.onClick(videoUrl)
+            }
+        })
+        var context = holder.videoIndexTv.context
+        if (selectedPosition == position) {
+            holder.itemView.background = context.getDrawable(R.drawable.detail_index_selected)
+        } else {
+            holder.itemView.background = context.getDrawable(R.drawable.detail_index_border)
+        }
     }
 
     class CategoryPageViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
