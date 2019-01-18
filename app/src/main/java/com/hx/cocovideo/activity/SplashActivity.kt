ab1003e3d96eb31e08c45d798bc35f4b118dc667
zhangheng <487225093@qq.com>
2019-01-18 11:46:30 +0800

    添加Splash页面

---
+++ app/src/main/java/com/hx/cocovideo/activity/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.hx.cocovideo.activity
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.hx.cocovideo.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed(object : Runnable {
+            override fun run() {
+                val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+        }, 5000)
+    }
+}
