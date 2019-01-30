6760891c95ab27309195c91c1dd428a7da73b238
zhangheng <487225093@qq.com>
2019-01-30 16:21:49 +0800

    完成注册页面的UI

--- app/src/main/java/com/hx/cocovideo/activity/LoginActivity.kt
+++ app/src/main/java/com/hx/cocovideo/activity/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.hx.cocovideo.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import com.hx.cocovideo.R
 
 
@@ -10,5 +12,10 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun onRegisterClick(view: View) {
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/hx/cocovideo/activity/RegisterActivity.kt
@@ -0,0 +1,18 @@
+package com.hx.cocovideo.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.hx.cocovideo.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+
+    fun onRegisterButtonClick(view: View) {
+
+    }
+}
