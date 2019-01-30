92e75ec1cae2ea41058ffc43cde177fdb7748cdb
zhangheng <487225093@qq.com>
2019-01-30 14:48:13 +0800

    完成登录页面的UI

---
+++ app/src/main/java/com/hx/cocovideo/activity/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.hx.cocovideo.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.hx.cocovideo.R
+
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
